0c3cf718a188c7c577922b247fa557c2e612f90c
Jekabs Gritans <jekabs@gritans.lv>
2025-07-25 22:34:33 +0300

    upd

--- provision.sh
+++ provision.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 echo "Script running" > /workspace/test.log
 
-tmux new-session -d -s aim 'bash -lc "
+# Determine setup.sh arguments based on environment variables
+SETUP_ARGS=""
+if [ "${SKIP_CUGRAPH}" = "true" ] || [ "${SKIP_CUGRAPH}" = "1" ]; then
+    SETUP_ARGS="--skip-cugraph"
+fi
+
+tmux new-session -d -s aim "bash -lc \"
   git config --global credential.helper store &&
-  echo \"https://jekabsgritans:${REPO_PAT}@github.com\" > ~/.git-credentials &&
+  echo \\\"https://jekabsgritans:\${REPO_PAT}@github.com\\\" > ~/.git-credentials &&
   chmod 600 ~/.git-credentials &&
   sudo apt update && sudo apt install -y neovim &&
   cd /workspace &&
@@ -11,8 +17,7 @@ tmux new-session -d -s aim 'bash -lc "
   cd rnn_dfa &&
   source /venv/main/bin/activate &&
   pip install --upgrade setuptools &&
-  ./setup.sh &&
+  ./setup.sh ${SETUP_ARGS} &&
   aim init &&
   aim up --host 0.0.0.0
-"'
-
+\""
